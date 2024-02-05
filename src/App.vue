472ca3305b47d875a36177657d7fca3e9f681cd9
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-05 14:25:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+  // import something from './components/';
+  export default {
+    name: ''
+  }
+</script>
+
+<template>
+    <h1>ciao</h1>
+</template>
+
+<style scoped>
+  /* style here */
+</style>
